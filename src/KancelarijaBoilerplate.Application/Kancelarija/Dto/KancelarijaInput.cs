e874c2f8f9cab8ec6a50dea4f9af0bb1b1a3d960
dejanjaredic <dejan.jaredic@bild-studio.net>
2019-03-26 21:38:40 +0100

    Dodat Dto

---
+++ src/KancelarijaBoilerplate.Application/Kancelarija/Dto/KancelarijaInput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KancelarijaBoilerplate.Kancelarija.Dto
+{
+    class KancelarijaInput
+    {
+        public string Opis { get; set; }
+    }
+}
---
+++ src/KancelarijaBoilerplate.Application/Osoba/Dto/OsobaOutpot.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KancelarijaBoilerplate.Osoba
+{
+    public class OsobaOutpot
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        
+    }
+}
