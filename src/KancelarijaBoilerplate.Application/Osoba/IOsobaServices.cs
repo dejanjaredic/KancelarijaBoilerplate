6daee655be0ba9c287113d64c6a4d6defd8bfab2
dejanjaredic <de3jan.jaredic@bild-studio.net>
2019-03-26 16:03:08 +0100

    second commit

---
+++ src/KancelarijaBoilerplate.Application/Osoba/IOsobaServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KancelarijaBoilerplate.Osoba
+{
+    class IOsobaServices
+    {
+    }
+}
---
+++ src/KancelarijaBoilerplate.Application/Uredjaj/IUredjajServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KancelarijaBoilerplate.Uredjaj
+{
+    class IUredjajServices
+    {
+    }
+}
